639ff7115c787dc9bd5e7f79f32f9ba39cfd3b7a
Katerin <katerinmonguacastro@gmail.com>
2024-03-02 00:23:36 -0500

    Starting project

---
+++ script.js
@@ -0,0 +1,16 @@
+// These are the global variables that can be modified. Some are going to work as indexes and others will be values as numbers.
+
+let xp = 0;
+let gold = 50;
+let health = 100;
+let fuel = 100;
+let currentWeapon = 0;
+let fighting;
+let inventory = ["laser"];
+let alienHealth;
+
+// We need to manipulate the DOM with the querySelector() method.
+
+let button1 = document.querySelector("#button1");
+let button2 = document.querySelector("#button2");
+let button3 = document.querySelector("#button3");
