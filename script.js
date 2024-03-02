8f3405b201f3f2f464a3ddae20490465ada9b673
Katerin <katerinmonguacastro@gmail.com>
2024-03-02 00:53:14 -0500

    Adding styles and the layout structure on the html and css files

--- script.js
+++ script.js
@@ -11,6 +11,6 @@ let alienHealth;
 
 // We need to manipulate the DOM with the querySelector() method.
 
-let button1 = document.querySelector("#button1");
-let button2 = document.querySelector("#button2");
-let button3 = document.querySelector("#button3");
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
