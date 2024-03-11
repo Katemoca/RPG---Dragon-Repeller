23f5be541b699546b34d51818c4234d56c494a79
Katerin <katerinmonguacastro@gmail.com>
2024-03-10 23:35:03 -0400

    Styles done

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # RPG-DragonRepeller
+
+![Static Badge](https://img.shields.io/badge/css-brightgreen)
+![Static Badge](https://img.shields.io/badge/html-%234F6CEA)
+![Static Badge](https://img.shields.io/badge/JavaScript-%23F55C04)
+![Static Badge](https://img.shields.io/badge/FreeCodeCamp-%2313416F)
+
 This is a simple text-based RPG game created using JavaScript and HTML. The game features a player who can move between different locations, fight monsters, buy items in a store, and win or lose the game based on their actions. This game was taken from the freeCodeCamp recent beta course of JavaScript.
+
+## Why this project?
+
+This is one of those projects you are looking for when you want to learn something new, but don't know where to start. It's the best oportunity to begin with JavaScript.
+
+## Credits
+
+This is all based on the FreeCodeCamp project to learn the basics of JavaScript.
--- script.js
+++ script.js
@@ -4,10 +4,10 @@ let xp = 0;
 let gold = 50;
 let health = 100;
 let fuel = 100;
-let currentWeapon = 0;
-let fighting;
-let inventory = ["laser"];
+let currentWeapon = 0; // The value of the currentWeapon variable corresponds to an index in the weapons array.
 let alienHealth;
+let fighting; // The value of this is the index of the current alien.
+let inventory = ["laser"];
 
 // We need to manipulate the DOM with the querySelector() method.
 // We're using the CONST variable as these variables are not going to change their values.
@@ -18,11 +18,11 @@ const text = document.querySelector("#text");
 const xpText = document.querySelector("#xpText");
 const healthText = document.querySelector("#healthText");
 const goldText = document.querySelector("#goldText");
-const monsterStats = document.querySelector("#monsterStats");
-const monsterName = document.querySelector("#monsterName");
-const monsterHealthText = document.querySelector("#monsterHealth");
+const alienStats = document.querySelector("#alienStats");
+const alienName = document.querySelector("#alienName");
+const alienHealthText = document.querySelector("#alienHealth");
 
-// We're going to arrange all our static data in arrays that are defined and passed through the constructor function below so that we can easily access.
+// We're going to arrange all our static data in arrays that are defined and passed through the constructor function below so that we can easily get access.
 
 const weapons = [
   { name: "laser", power: 5 },
@@ -31,11 +31,31 @@ const weapons = [
   { name: "sword", power: 100 },
 ];
 
+// This is an array of the aliens we need to fight against.
+const aliens = [
+  {
+    name: "muguls",
+    level: 2,
+    health: 15,
+  },
+  {
+    name: "alien beast",
+    level: 8,
+    health: 60,
+  },
+  {
+    name: "alien destructor",
+    level: 20,
+    health: 300,
+  },
+];
+
+// This is an array of the locations with the information that each one needs.
 const locations = [
   {
     name: "space station",
-    "button text": ["Go to store", "Go to cave", "Fight dragon"],
-    "button functions": [goStore, goPlanet, fightAlien],
+    "button text": ["Go to space store", "Go to planet", "Fight alien"],
+    "button functions": [goStore, goPlanet, fightAlienDestructor],
     text: 'You are in the space station. You see a sign that says "Space Store".',
   },
   {
@@ -50,72 +70,95 @@ const locations = [
   },
   {
     name: "planet",
-    "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
-    "button functions": [fightPixins, fightAlienBeast, goStation],
+    "button text": ["Fight muguls", "Fight alien beast", "Go to town square"],
+    "button functions": [fightMuguls, fightAlienBeast, goStation],
     text: "You enter the planet. You see some aliens.",
   },
   {
     name: "fight",
     "button text": ["Attack", "Dodge", "Run"],
     "button functions": [attack, dodge, goStation],
-    text: "You are fighting a monster.",
+    text: "You are fighting an alien.",
+  },
+  {
+    name: "kill alien",
+    "button text": [
+      "Go to space station",
+      "Go to space station",
+      "Go to space station",
+    ],
+    "button functions": [goStation, goStation, easterEgg],
+    text: 'The alien screams "Arg!" as it dies. You gain experience points and find gold.',
+  },
+  {
+    name: "lose",
+    "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
+    "button functions": [restart, restart, restart],
+    text: "You die. &#x2620;",
+  },
+  {
+    name: "win",
+    "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
+    "button functions": [restart, restart, restart],
+    text: "You defeat the dragon! YOU WIN THE GAME! &#x1F389;",
+  },
+  {
+    name: "easter egg",
+    "button text": ["2", "8", "Go to space station?"],
+    "button functions": [pickTwo, pickEight, goStation],
+    text: "You find a secret game. Pick a number above. Ten numbers will be randomly chosen between 0 and 10. If the number you choose matches one of the random numbers, you win!",
   },
 ];
 
-const aliens = [];
-
-// We need to initialize the buttons with the onclick event.
-button1.onclick = goStore;
-button2.onclick = goPlanet;
-button3.onclick = fightAlien;
+// We need to initialize the buttons with the onclick event. We're defining FUNCTIONS for these button actions.
+button1.onclick = goStore; // Buying
+button2.onclick = goPlanet; // Fighting aliens
+button3.onclick = fightAlienDestructor; // Fighting ultimate alien
 
 // We need to use a function to update the location wer're at and display its text. This is to prevent code redundancy and to modularize our code.
-
-const update = (location) => {
+function update(location) {
   button1.innerText = location["button text"][0];
   button2.innerText = location["button text"][1];
   button3.innerText = location["button text"][2];
   button1.onclick = location["button functions"][0];
   button2.onclick = location["button functions"][1];
   button3.onclick = location["button functions"][2];
-  text.innerHtml = location.text;
-};
+  text.innerHTML = location.text;
+}
 
-// We start creating functions to make our game functional
-const goStation = () => {
+// We start creating functions to UTILIZE the ARRAY "locations" to set the place where the actions are going to occur.
+function goStation() {
   update(locations[0]);
-};
-
-const goStore = () => {
+}
+function goStore() {
   update(locations[1]);
-};
-const goPlanet = () => {
+}
+function goPlanet() {
   update(locations[2]);
-};
-const fightAlien = () => {
-  console.log("Fight alien.");
-};
+}
 
-const buyHealth = () => {
-  if (gold >= 5) {
-    gold -= 5;
-    health += 5;
+// These are the functions to control the ACTIONS the player is allowed to perform.
+function buyHealth() {
+  if (gold >= 10) {
+    gold -= 10;
+    health += 10;
     goldText.innerText = gold;
     healthText.innerText = health;
   } else {
     text.innerHTML = "You don't have enough gold to buy health.";
   }
-};
-const buyWeapon = () => {
+}
+
+function buyWeapon() {
   if (currentWeapon < weapons.length - 1) {
-    if (gold >= 25) {
-      gold -= 25;
+    if (gold >= 30) {
+      gold -= 30;
       currentWeapon++;
       goldText.innerText = gold;
-      let newWeapon = weapons[currentWeapon];
-      text.innerHTML = "You now have a new weapon: " + newWeapon.name;
+      let newWeapon = weapons[currentWeapon].name;
+      text.innerHTML = "You now have a new weapon: " + newWeapon + ".";
       inventory.push(newWeapon);
-      text.innerText += " In your inventory you have: " + inventory;
+      text.innerText += " In your inventory you have: " + inventory + ".";
     } else {
       text.innerHTML = "You do not have enough gold to buy a weapon.";
     }
@@ -124,9 +167,9 @@ const buyWeapon = () => {
     button2.innerText = "Sell weapon for 15 gold";
     button2.onclick = sellWeapon;
   }
-};
+}
 
-const sellWeapon = () => {
+function sellWeapon() {
   if (inventory.length > 1) {
     gold += 15;
     goldText.innerText = gold;
@@ -136,10 +179,136 @@ const sellWeapon = () => {
   } else {
     text.innerHTML = "Don't sell your only weapon!";
   }
-};
-const fightPixins = () => {};
-const fightAlienBeast = () => {};
-const fightAlienDestructor = () => {};
-const goFight = () => {};
-const attack = () => {};
-const dodge = () => {};
+}
+
+// Player actions to fight against a specific ALIEN from the array of aliens.
+function fightMuguls() {
+  fighting = 0;
+  goFight();
+}
+
+function fightAlienBeast() {
+  fighting = 1;
+  goFight();
+}
+
+function fightAlienDestructor() {
+  fighting = 2;
+  goFight();
+}
+
+// Helper functions of the fighting actions
+function goFight() {
+  update(locations[3]);
+  alienHealth = aliens[fighting].health;
+  alienStats.style.display = "block";
+  alienName.innerText = aliens[fighting].name;
+  alienHealthText.innerText = alienHealth;
+}
+
+function attack() {
+  text.innerText = "You're being attacked by" + aliens[fighting].name + "!";
+  text.innerText = " You attack with your " + weapons[currentWeapon].name + ".";
+  health -= getAlienAttackValue(aliens[fighting].level);
+  if (isAlienHit()) {
+    alienHealth -=
+      weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;
+  } else {
+    text.innerText += " You miss.";
+  }
+  healthText.innerText = health;
+  alienHealthText.innerText = alienHealth;
+  if (health <= 0) {
+    lose();
+  } else if (alienHealth <= 0) {
+    if (fighting === 2) {
+      winGame();
+    }
+  } else {
+    defeatAlien();
+  }
+  if (Math.random() <= 0.1 && inventory.length !== 1) {
+    text.innerText += " Your " + inventory.pop() + " breaks.";
+    currentWeapon--;
+  }
+}
+
+// This will set the monster's attack to five times their level minus a random number between 0 and the player's xp.
+function getAlienAttackValue(level) {
+  const hit = level * 5 - Math.floor(Math.random() * xp);
+  console.log(hit);
+  return hit > 0 ? hit : 0;
+}
+
+function isAlienHit() {
+  return Math.random() > 0.2 || health < 20;
+}
+
+function dodge() {
+  text.innerText = "You dodge the attack from the " + aliens[fighting].name;
+}
+function defeatAlien() {
+  gold += Math.floor(aliens[fighting].level * 6.7);
+  xp += aliens[fighting].level;
+  goldText.innerText = gold;
+  xpText.innerText = xp;
+  update(locations[4]);
+}
+
+// Implement win and lose conditions to determine the outcome of the game based on player actions.
+function lose() {
+  update(locations[5]);
+}
+function winGame() {
+  update(locations[6]);
+}
+
+// Define a function to reset player stats and restart the game.
+function restart() {
+  xp = 0;
+  health = 100;
+  gold = 50;
+  currentWeapon = 0;
+  inventory = ["laser"];
+  goldText.innerText = gold;
+  healthText.innerText = health;
+  xpText.innerText = xp;
+  goStation();
+}
+
+// This is a HIDDEN feature of the game.
+
+function easterEgg() {
+  update(locations[7]);
+}
+
+function pickTwo() {
+  pick(2);
+}
+function pickEight() {
+  pick(8);
+}
+
+function pick(guess) {
+  const numbers = [];
+
+  while (numbers.length < 10) {
+    numbers.push(Math.floor(Math.random() * 11));
+  }
+  text.innerText = "You picked " + guess + ". Here are the random numbers:\n";
+  for (let i = 0; i < 10; i++) {
+    text.innerHTML += numbers[i] + "\n";
+  }
+  if (numbers.includes(guess)) {
+    text.innerHTML += "Right! You win 20 gold!";
+    gold += 20;
+    goldText.innerText = gold;
+  } else {
+    text.innerText += "Wrong! You lose 10 health!";
+    health -= 10;
+    healthText.innerText = health;
+    if (health <= 0) {
+      lose();
+    }
+  }
+}
